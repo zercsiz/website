b0de7dbd10c39bcd87ff631222d8960e8e93bca7
zercsiz <ksourmi@gmail.com>
2023-03-15 20:30:40 +0330

    fixed a problem with teachers query and wrote teacher_cards.html file

--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from accounts.models import Account
 
 
 def home_view(request):
-    teacher_list = Account.objects.get(is_teacher=True)[:6]
+
+    teacher_list = Account.objects.get(is_teacher=True)
     course_list = Course.objects.all()[:6]
     context = {'courses': course_list, 'teachers': teacher_list}
     return render(request, 'home/home.html', context)
