17399193cd205d4fe9c336ca95c8de1f0165ed09
zercsiz <ksourmi@gmail.com>
2023-03-15 19:52:01 +0330

    connected courses to cards in homepage

--- home/models.py
+++ home/models.py
@@ -17,7 +17,6 @@ class Course(models.Model):
     days = models.CharField(max_length=200)
     hour = models.CharField(max_length=200)
     language = models.CharField(max_length=1, choices=languages)
-    mamad = models.CharField(max_length=250,blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} | Language:{self.language} | Start Date:{self.start_date} | {self.description[:60]}... | Days:{self.created} | Hour:{self.edited}"
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Course
 
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    course_list = Course.objects.all()[:6]
+    return render(request, 'home/home.html', {'courses': course_list})
+
