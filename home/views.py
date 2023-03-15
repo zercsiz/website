7b42cd99bd91d1b2695dc02f766473e610a810f8
zercsiz <ksourmi@gmail.com>
2023-03-15 20:17:21 +0330

    aditted home>views.py to get a list of teachers as context

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Course
+from accounts.models import Account
 
 
 def home_view(request):
+    teacher_list = Account.objects.get(is_teacher=True)[:6]
     course_list = Course.objects.all()[:6]
-    return render(request, 'home/home.html', {'courses': course_list})
+    context = {'courses': course_list, 'teachers': teacher_list}
+    return render(request, 'home/home.html', context)
 
