50e9d59e7e001cecb4859c7860f5c79faa28df8d
zercsiz <ksourmi@gmail.com>
2023-03-16 23:11:13 +0330

    removed condition of homepage

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,15 +1,10 @@
 
 const navFl = document.querySelector('.navbar')
-const winHref = window.location.href
 
-if (winHref === 'http://127.0.0.1:8000') {
-    window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
-            navFl.classList.add('navbar-scrolled');
+        navFl.classList.add('navbar-scrolled');
     } else if (window.scrollY < 56) {
-            navFl.classList.remove('navbar-scrolled')
+        navFl.classList.remove('navbar-scrolled')
     }
-    })
-} else {
-    navFl.classList.add('navbar-scrolled')
-}
+})
