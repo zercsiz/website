393edbf4bf1465ef8dfb3f6916f4288e0011c148
zercsiz <ksourmi@gmail.com>
2023-03-16 20:14:52 +0330

    slight changes in navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,8 +3,8 @@ const navFl = document.querySelector('.navbar')
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
-        navFl.classList.add('navbar-scrolled');
+            navFl.classList.add('navbar-scrolled');
     } else if (window.scrollY < 56) {
-        navFl.classList.remove('navbar-scrolled')
+            navFl.classList.remove('navbar-scrolled')
     }
 })
