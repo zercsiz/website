bf6e98b2921be7f88cda966af54757d043bf176e
zercsiz <ksourmi@gmail.com>
2023-03-16 21:53:24 +0330

    made create teacher times view

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . import models
+from .models import TeacherTime
 from datetime import date, timedelta, datetime
 
 
@@ -11,14 +11,17 @@ def create_time(request):
 
     if request.method == "POST":
         date_list = []
+        teacher_time_list = request.POST.getlist('times')
+
         start_date = date.today()
         end_date = date(2024, 1, 1)
-        for single_date in daterange(start_date, end_date):
-            date_list.append(single_date.strftime("%Y-%m-%d"))
-            print(date_list)
 
-        time = request.POST.getlist('times')
-        print(time)
+        for single_date in daterange(start_date, end_date):
+            d = single_date.strftime("%Y-%m-%d")
+            for t in teacher_time_list:
+                if single_date.weekday() == int(t[0]):
+                    t_time = TeacherTime.objects.create(date=d, start=t[2:])
+                    t_time.save()
 
         return redirect('account_details')
     return render(request, 'courses/time_checkbox.html')
