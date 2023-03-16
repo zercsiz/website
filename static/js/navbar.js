26306b9acfcf88f1e143162708dfefa44512701e
zercsiz <ksourmi@gmail.com>
2023-03-16 20:38:18 +0330

    added homepage condition to navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,10 +1,15 @@
 
 const navFl = document.querySelector('.navbar')
+const winHref = window.location.href
 
-window.addEventListener('scroll', () => {
+if (winHref === 'http://127.0.0.1:8000') {
+    window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
             navFl.classList.add('navbar-scrolled');
     } else if (window.scrollY < 56) {
             navFl.classList.remove('navbar-scrolled')
     }
-})
+    })
+} else {
+    navFl.classList.add('navbar-scrolled')
+}
