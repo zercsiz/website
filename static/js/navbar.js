d4f359b58df563bf33301b7d3705d677ca7ca47f
zercsiz <ksourmi@gmail.com>
2023-03-16 23:55:05 +0330

    made some of pages farsi

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,10 +1,11 @@
 
 const navFl = document.querySelector('.navbar')
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 56) {
-        navFl.classList.add('navbar-scrolled');
-    } else if (window.scrollY < 56) {
-        navFl.classList.remove('navbar-scrolled')
-    }
-})
+window.addEventListener('scroll',
+    () => {
+        if (window.scrollY >= 56) {
+            navFl.classList.add('navbar-scrolled');
+        } else if (window.scrollY < 56) {
+            navFl.classList.remove('navbar-scrolled');
+        }
+    });
