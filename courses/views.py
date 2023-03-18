632edfaea17db497bb77492acfdba142373fd333
zercsiz <ksourmi@gmail.com>
2023-03-18 17:44:44 +0330

    restricted the create_time view to the is_teacher

--- courses/views.py
+++ courses/views.py
@@ -4,35 +4,37 @@ from datetime import date, timedelta, datetime
 
 
 def create_time(request):
+    if request.user.is_teacher:
+        def daterange(start_date, end_date):
+            for n in range(int((end_date - start_date).days)):
+                yield start_date + timedelta(n)
 
-    def daterange(start_date, end_date):
-        for n in range(int((end_date - start_date).days)):
-            yield start_date + timedelta(n)
+        #this function adds 1:30 to start time
+        def calculate_endtime(start):
+            start = start
+            hour = int(start[0:2])
+            minute = int(start[3:])
+            end = timedelta(hours=hour, minutes=minute) + timedelta(hours=1, minutes=30)
+            return end
 
-    #this function adds 1:30 to start time
-    def calculate_endtime(start):
-        start = start
-        hour = int(start[0:2])
-        minute = int(start[3:])
-        end = timedelta(hours=hour, minutes=minute) + timedelta(hours=1, minutes=30)
-        return end
+        if request.method == "POST":
+            teacher_time_list = request.POST.getlist('times')
 
-    if request.method == "POST":
-        teacher_time_list = request.POST.getlist('times')
+            start_date = date.today()
+            end_date = date(2023, 12, 1)
 
-        start_date = date.today()
-        end_date = date(2023, 12, 1)
+            for single_date in daterange(start_date, end_date):
+                d = single_date.strftime("%Y-%m-%d")
+                for t in teacher_time_list:
+                    end_time = str(calculate_endtime(t[2:]))
+                    if single_date.weekday() == int(t[0]):
+                        t_time = TeacherTime.objects.create(date=d, start=t[2:], end=end_time)
+                        t_time.save()
 
-        for single_date in daterange(start_date, end_date):
-            d = single_date.strftime("%Y-%m-%d")
-            for t in teacher_time_list:
-                end_time = str(calculate_endtime(t[2:]))
-                if single_date.weekday() == int(t[0]):
-                    t_time = TeacherTime.objects.create(date=d, start=t[2:], end=end_time)
-                    t_time.save()
-
-        return redirect('account_details')
-    return render(request, 'courses/time_checkbox.html')
+            return redirect('account_details')
+        return render(request, 'courses/time_checkbox.html')
+    else:
+        return redirect('home')
 
 
 
