01dbb3b35699479071d60011db315a6d03c7c15b
zercsiz <ksourmi@gmail.com>
2023-03-20 22:37:02 +0330

    edited the template name of password change and reset urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,15 +11,15 @@ urlpatterns = [
 
     #password reset urls
     path('password_change/done/',
-         views.auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         views.auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
          name='password_change_done'),
 
     path('password_change/',
-         views.auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         views.auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'),
          name='password_change'),
 
     path('password_reset/done/',
-         views.auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+         views.auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
          name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/',
@@ -28,7 +28,7 @@ urlpatterns = [
 
     path('reset/done/',
          views.auth_views.PasswordResetCompleteView.as_view
-         (template_name='registration/password_reset_complete.html'),
+         (template_name='accounts/password_reset_complete.html'),
          name='password_reset_complete'),
 
 ]
