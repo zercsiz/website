14738cf2a52e32fa1b4a31d3acd91da57ae7b782
zercsiz <ksourmi@gmail.com>
2023-04-01 18:19:24 +0330

    created accordion for months

--- courses/views.py
+++ courses/views.py
@@ -64,10 +64,23 @@ class CreateTime(LoginRequiredMixin, View):
 
 class TeacherDetails(View):
     def get(self, request, teacher_id):
+        months = {1: 'فروردین',
+                  2: 'اردیبهشت',
+                  3: 'خرداد',
+                  4: 'تیر',
+                  5: 'مرداد',
+                  6: 'شهریور',
+                  7: 'مهر',
+                  8: 'آبان',
+                  9: 'آذر',
+                  10: 'دی',
+                  11: 'بهمن',
+                  12: 'اسفند'}
         teacher_time = TeacherTime.objects.filter(teacher=teacher_id)
         teacher = Account.objects.get(id=teacher_id)
         context = {
             'teacher': teacher,
-            'teacher_time': teacher_time
+            'teacher_time': teacher_time,
+            'months': months,
         }
         return render(request, 'courses/teacher_details.html', context)
