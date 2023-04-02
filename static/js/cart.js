ddd5096b163e48c9d818c075ef8d3cd6ca56dee5
zercsiz <ksourmi@gmail.com>
2023-04-02 23:45:36 +0330

    modified js to get id of teacherTime but for now id is undefined in the console i should fix it

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,10 @@
-console.log('hello motherfucker')
+var updateBtns = document.getElementsByClassName('update-cart')
+
+
+for(var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var teacherTimeId = this.dataset.teacherTime
+        var action = this.dataset.action
+        console.log('productId:', teacherTimeId, "action:", action)
+    })
+}
