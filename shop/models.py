5511ec1fe9ba834ed1427c316ed7757cdf3807b5
zercsiz <ksourmi@gmail.com>
2023-04-04 00:10:56 +0330

    added Order model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="Student")
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return f"{self.student} - {self.transaction_id}"
