6da54e59cb64d8381d76565b486699017a709d84
zercsiz <ksourmi@gmail.com>
2023-04-06 22:23:35 +0330

    changed sidebar appearance again

--- static/js/profile_base_sidebar.js
+++ static/js/profile_base_sidebar.js
@@ -1,28 +1,5 @@
-const sideNL1 = document.querySelector('.sidebar-nav-link1')
-const sideNL2 = document.querySelector('.sidebar-nav-link2')
-const sideNL3 = document.querySelector('.sidebar-nav-link3')
-const sideNL4 = document.querySelector('.sidebar-nav-link4')
-
-
-if(window.location.pathname==='/accounts/details/') {
-    sideNL1.classList.add('nav-link-active');
-    sideNL2.classList.add('nav-link-deactive');
-    sideNL3.classList.add('nav-link-deactive');
-    sideNL4.classList.add('nav-link-deactive');
-
-
-}
-if (window.location.pathname==='/courses/time_checkbox') {
-    sideNL2.classList.add('nav-link-active');
-    sideNL1.classList.add('nav-link-deactive');
-    sideNL3.classList.add('nav-link-deactive');
-    sideNL4.classList.add('nav-link-deactive');
-
-}
-if (window.location.pathname==='/accounts/edit/') {
-    sideNL3.classList.add('nav-link-active');
-    sideNL2.classList.add('nav-link-deactive');
-    sideNL1.classList.add('nav-link-deactive');
-    sideNL4.classList.add('nav-link-deactive');
-
-}
+document.querySelectorAll('.sidebar-link').forEach(link => {
+    if(link.href === window.location.href){
+        link.setAttribute('aria-current', 'page')
+    }
+})
