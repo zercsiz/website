e82907d4d11a49d846ab3f501412889f12aa4e00
zercsiz <ksourmi@gmail.com>
2023-04-19 23:19:44 +0330

    shows 6 of teachers

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from accounts.models import Account
 
 def home_view(request):
 
-    teacher_list = Account.objects.filter(is_teacher=True).exclude(slug__exact="")
+    teacher_list = Account.objects.filter(is_teacher=True).exclude(slug__exact="")[:6]
     course_list = Course.objects.all()[:6]
     context = {'courses': course_list, "teachers": teacher_list}
     return render(request, 'home/home.html', context)
