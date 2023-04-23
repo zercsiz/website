6971b1165b705d353bb75ec51ecfba4b59aa2f87
zercsiz <ksourmi@gmail.com>
2023-04-23 20:13:43 +0330

    got rid of useless view

--- shop/views.py
+++ shop/views.py
@@ -27,19 +27,6 @@ class CartView(View):
         return render(request, 'shop/cart.html', context)
 
 
-
-def updateItem(request):
-    data = json.loads(request.body)
-    teacherTimeId = data['teacherTimeId']
-    action = data['action']
-    student = request.user
-    teacherTime = TeacherTime.objects.get(id=teacherTimeId)
-    order, created = Order.objects.get_or_create(student=student, complete=False)
-    orderItem, created = OrderItem.objects.get_or_create(order=order, teacherTime=teacherTime)
-    messages.success(request, "کلاس به سبد خرید شما اضافه شد", 'success')
-    return JsonResponse("Item was added", safe=False)
-
-
 class removeItem(View):
     def get(self, request, item_id):
         order_item = OrderItem.objects.get(id=item_id)
