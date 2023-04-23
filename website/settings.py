86def4fde33871474dab2a2fb7886784e9c9d0e1
zercsiz <ksourmi@gmail.com>
2023-04-24 02:14:15 +0330

    added a page for 404 error

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)5qy^5=(_b3d!s06ziy35f*h8vj8^h$%n6*nim^e++!bf7aw_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # only during development
