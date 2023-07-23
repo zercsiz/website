77642a68705e895000f3b1ee255bcf6d7911f0f3
zercsiz <ksourmi@gmail.com>
2023-07-23 18:50:30 +0300

    admin panel customization

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import *
+from . import models
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('student', 'date_ordered', 'complete', 'transaction_id', 'total')
+    list_filter = ('complete',)
+    search_fields = ('student', 'transaction_id')
+    raw_id_fields = ('student',)
+
+
+@admin.register(models.OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('teacherTime', 'order', 'date_added')
+    search_fields = ('order', 'teacherTime')
 
